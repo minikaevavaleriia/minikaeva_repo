c160b40bb53292a45c9c7bdb0795db7123b552f3
minikaevavaleriia <valeriia@minikaeva.ru>
2023-02-08 12:00:47 +0300

    OS_module_hw

--- SYSTEM_OS_HW/2.py
+++ SYSTEM_OS_HW/2.py
@@ -11,3 +11,4 @@ if not os.path.exists(newpath):
 for i in range(1, 11):
     if not os.path.exists(newpath + r'/' + str(i)):
         os.makedirs(newpath + r'/' + str(i))
+    os.system(f"echo текст, чтобы папки залились на гит > {newpath + '/' + str(i) + '/text.txt'}")
