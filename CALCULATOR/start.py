a3747cab2fb7ce5c7c037da59be697baf6c6da48
minikaevavaleriia <valeriia@minikaeva.ru>
2023-02-12 00:52:42 +0300

    calculator

--- CALCULATOR/start.py
+++ CALCULATOR/start.py
@@ -20,9 +20,8 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.memory = []
-
-
         self.setMouseTracking(True)
+
         # окно
         self.setWindowTitle("INSTACALC")
         self.setFixedSize(QSize(720, 720))
@@ -36,57 +35,50 @@ class MainWindow(QMainWindow):
 
         #цифры
         self.number_1 = NumberButton('1', 1)
-        self.number_1.clicked.connect(lambda: self.number_was_clicked(self.number_1.name))
-
         self.number_2 = NumberButton('2', 2)
-        self.number_2.clicked.connect(lambda: self.number_was_clicked(self.number_2.name))
-
         self.number_3 = NumberButton('3', 3)
-        self.number_3.clicked.connect(lambda: self.number_was_clicked(self.number_3.name))
-
         self.number_4 = NumberButton('4', 4)
-        self.number_4.clicked.connect(lambda: self.number_was_clicked(self.number_4.name))
-
         self.number_5 = NumberButton('5', 5)
-        self.number_5.clicked.connect(lambda: self.number_was_clicked(self.number_5.name))
-
         self.number_6 = NumberButton('6', 6)
-        self.number_6.clicked.connect(lambda: self.number_was_clicked(self.number_6.name))
-
         self.number_7 = NumberButton('7', 7)
-        self.number_7.clicked.connect(lambda: self.number_was_clicked(self.number_7.name))
-
         self.number_8 = NumberButton('8', 8)
-        self.number_8.clicked.connect(lambda: self.number_was_clicked(self.number_8.name))
-
         self.number_9 = NumberButton('9', 9)
-        self.number_9.clicked.connect(lambda: self.number_was_clicked(self.number_9.name))
-
         self.number_0 = NumberButton('0', 0)
-        self.number_0.clicked.connect(lambda: self.number_was_clicked(self.number_0.name))
+
+
+        self.number_1.clicked.connect(lambda: self.button_was_clicked(self.number_1.name))
+        self.number_2.clicked.connect(lambda: self.button_was_clicked(self.number_2.name))
+        self.number_3.clicked.connect(lambda: self.button_was_clicked(self.number_3.name))
+        self.number_4.clicked.connect(lambda: self.button_was_clicked(self.number_4.name))
+        self.number_5.clicked.connect(lambda: self.button_was_clicked(self.number_5.name))
+        self.number_6.clicked.connect(lambda: self.button_was_clicked(self.number_6.name))
+        self.number_7.clicked.connect(lambda: self.button_was_clicked(self.number_7.name))
+        self.number_8.clicked.connect(lambda: self.button_was_clicked(self.number_8.name))
+        self.number_9.clicked.connect(lambda: self.button_was_clicked(self.number_9.name))
+        self.number_0.clicked.connect(lambda: self.button_was_clicked(self.number_0.name))
 
 
         #кнопки с действиями
         self.action_AC = ActionButton('AC', 'del')
-        self.action_AC.clicked.connect(lambda: self.number_was_clicked(self.action_AC.name))
+        self.action_AC.clicked.connect(lambda: self.button_was_clicked(self.action_AC.name))
 
         self.action_MS = ActionButton('MS', 'save')
         self.action_MS.clicked.connect(lambda: self.memory_save(self.action_MS.name))
 
         self.action_power = ActionButton('^', 'pow')
-        self.action_power.clicked.connect(lambda: self.number_was_clicked(self.action_power.name))
+        self.action_power.clicked.connect(lambda: self.button_was_clicked(self.action_power.name))
 
         self.action_div = ActionButton('/', 'div')
-        self.action_div.clicked.connect(lambda: self.number_was_clicked(self.action_div.name))
+        self.action_div.clicked.connect(lambda: self.button_was_clicked(self.action_div.name))
 
         self.action_multiply = ActionButton('*', 'multiply')
-        self.action_multiply.clicked.connect(lambda: self.number_was_clicked(self.action_multiply.name))
+        self.action_multiply.clicked.connect(lambda: self.button_was_clicked(self.action_multiply.name))
 
         self.action_sub = ActionButton('-', 'sub')
-        self.action_sub.clicked.connect(lambda: self.number_was_clicked(self.action_sub.name))
+        self.action_sub.clicked.connect(lambda: self.button_was_clicked(self.action_sub.name))
 
         self.action_add = ActionButton('+', 'add')
-        self.action_add.clicked.connect(lambda: self.number_was_clicked(self.action_add.name))
+        self.action_add.clicked.connect(lambda: self.button_was_clicked(self.action_add.name))
 
         self.action_eq = ActionButton('=', 'eq')
         self.action_eq.clicked.connect(lambda: self.equals())
@@ -101,9 +93,10 @@ class MainWindow(QMainWindow):
         #кнопки с цифрами располагаю
         self.layout = QGridLayout()
 
+
         # добавляю дисплей
-        # self.layout.addWidget(self.input)
         self.layout.addWidget(self.label_result)
+
 
         # расставляю цифры
         self.layout.addWidget(self.number_7, 2, 0, 1, 1)
@@ -136,16 +129,19 @@ class MainWindow(QMainWindow):
         self.layout.addWidget(self.action_dot, 5, 2)
         self.layout.addWidget(self.action_MR, 5, 3)
 
+
         #РАССТОЯНИЕ МЕЖДУ ЭЛЕМЕНТАМИ И ПИКСЕЛИ ОТ КРАЕВ ДО СЛОЯ НЕ ТРОГАААААТЬ!!!
         self.layout.setContentsMargins(100, 52, 100, 40)
         self.layout.setHorizontalSpacing(53)
+
 
         # создаю контейнер, в него лейаут и контейнер - центральный виджет
         self.container = QWidget()
         self.container.setLayout(self.layout)
         self.setCentralWidget(self.container)
 
-    def number_was_clicked(self, button):  # вывод цифры на кнопке на экран
+
+    def button_was_clicked(self, button):  # вывод цифры на кнопке на экран
         if button == 'AC':
             self.label_result.setText(f'0')
         else:
@@ -155,6 +151,7 @@ class MainWindow(QMainWindow):
             self.label_result.setText(f'{self.label_result.text()}{button}')
 
         print(f'{button} pushed!')
+
 
     def equals(self):
         screen = self.label_result.text()
@@ -169,6 +166,7 @@ class MainWindow(QMainWindow):
         except:
             self.label_result.setText("ERROR")
 
+
     def dot(self, button):
         screen = self.label_result.text()
         if screen[-1] == ".":
@@ -176,6 +174,7 @@ class MainWindow(QMainWindow):
         else:
             self.label_result.setText(f'{screen}.')
         print(f'{button} pushed!')
+
 
     def memory_save(self, button):
         screen = self.label_result.text()
@@ -186,6 +185,7 @@ class MainWindow(QMainWindow):
                 self.label_result.setText("MEMORY IS FULL")
         print(f'{button} pushed!')
 
+
     def memory_read(self, button):
         screen = self.label_result.text()
         if self.memory:
@@ -194,22 +194,24 @@ class MainWindow(QMainWindow):
             self.label_result.setText("MEMORY IS EMPTY")
         print(f'{button} pushed!')
 
-def check_power(screen):
+
+def check_power(screen):                        # доделать для отрицательных чисел и отрицательных степеней!!!
     match = r'\d+\^\d+'
     look_for_power = re.findall(match, screen)
 
     if look_for_power:
         for i in look_for_power:
             print('lalal ' + i)
-            x = re.findall(r'\d+', i[0])
+            x = re.findall(r'-?\d+', i[0])
             a = f'{x[0]}'
 
-            y = re.findall(r'\d+$', i)
+            y = re.findall(r'-?\d+$', i)
             b = f'{y[0]}'
 
             ans = pow(float(a), float(b))
 
             screen = screen.replace(i, str(ans))
+
             print('new screen ' + screen)
 
     return screen
@@ -243,7 +245,7 @@ def main():
         QLabel{
             background-color: rgba(0, 0, 0, 0.4);
             font-family: 'Lucida Console', Monaco, monospace;
-            font-size: 30px;
+            font-size: 60px;
             font-weight: bold;
             border-radius: 50%;
             border-style: solid;
